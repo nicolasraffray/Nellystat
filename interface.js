d0a30c557c534bfef854f346caf0f67d390328f9
nicolasraffray <nraffray@gmail.com>
2020-01-23 18:55:57 +0000

    hey tom

---
+++ interface.js
@@ -0,0 +1,40 @@
+$(document).ready(function (){
+  
+  var thermostat = new Thermostat();
+
+  $("#thermodisplay").text(thermostat.temperature);
+
+  $("#increase").click(function() {
+    thermostat.increase()
+    $("#thermodisplay").text(thermostat.temperature);
+  });
+
+  $("#decrease").click(function() {
+    thermostat.decrease()
+    $("#thermodisplay").text(thermostat.temperature);
+  });
+
+  $("#reset").click(function() {
+    thermostat.reset()
+    $("#thermodisplay").text(thermostat.temperature);
+  });
+
+  function updateTemperature(){
+    $('#temperature').text(thermostat.temperature)
+    $('#temperature').attr('class',thermostat.usage())
+  }
+
+  $('*:checkbox').click(function() {
+    if ($(this).is(":checked")) {
+        thermostat.toggle();
+    } else if ($(this).is(":not(:checked")){
+        thermostat.toggle()
+    }
+  });
+
+
+
+  $.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=1c91a074559e143366b0cc4f23ff3082&units=metric",function(data){
+      $('#current-temp').text(data.main.temp);
+  });
+});
